b84a337ed805df9e5a3be84e6c271d676b2155a9
Punit <punit.15884@gmail.com>
2025-06-14 10:17:05 +0530

    updated amy

--- src/main/java/com/punit/sts/NovaSonicVoipGateway.java
+++ src/main/java/com/punit/sts/NovaSonicVoipGateway.java
@@ -23,6 +23,8 @@ import org.zoolu.net.SocketAddress;
 
 import java.util.Map;
 
+import com.punit.sts.NovaMediaConfig;
+
 
 /**
  * VoIP Gateway/User Agent for Nova Sonic S2S.
@@ -122,7 +124,7 @@ public class NovaSonicVoipGateway extends RegisteringMultipleUAS {
         ServiceConfig serviceConfig = new ServiceConfig();
         NovaMediaConfig mediaConfig = new NovaMediaConfig();
         Map<String, String> environ = System.getenv();
-        mediaConfig.setNovaVoiceId(environ.getOrDefault("NOVA_VOICE_ID","en_us_matthew"));
+        mediaConfig.setNovaVoiceId(environ.getOrDefault("NOVA_VOICE_ID", "amy")); //en_gb_amy
         if (isConfigured(environ.get("NOVA_PROMPT"))) {
             mediaConfig.setNovaPrompt(environ.get("NOVA_PROMPT"));
         }
