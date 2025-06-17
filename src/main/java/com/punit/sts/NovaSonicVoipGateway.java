85fc185526d31dec301c5355f77a6cfe79fe33ad
Punit <punit.15884@gmail.com>
2025-06-18 00:47:08 +0530

    updated bit rate

--- src/main/java/com/punit/sts/NovaSonicVoipGateway.java
+++ src/main/java/com/punit/sts/NovaSonicVoipGateway.java
@@ -196,7 +196,7 @@ public class NovaSonicVoipGateway extends RegisteringMultipleUAS {
                 new MediaSpec[]{
                         new MediaSpec(0,
                                 "PCMU",
-                                16000,
+                                8000,
                                 1,
                                 160)})};
     }
